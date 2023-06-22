83afb89db6e54efd082f57513505346d83246f26
alain <alain.seys@outlook.com>
2023-06-22 13:59:03 +0200

    Dhcp backup script

---
+++ DHCP/Dhcp backup.ps1
@@ -0,0 +1,9 @@
+﻿Set-Location C:\biopack
+$new_folder = Get-Date -Format "dd_MM_yyyy_HH_mm"
+
+New-Item -ItemType Directory -Path C:\biopack\dhcpbackup\ -Name $new_folder
+
+Backup-DhcpServer -path C:\biopack\dhcpbackup\$new_folder
+
+Copy-Item C:\biopack\dhcpbackup\$new_folder\* -Destination \\WIN2019-DC\IT\Backups\dhcp\$new_folder\
+Remove-Item C:\biopack\dhcpbackup\$new_folder -Recurse -Force -Confirm:$false
