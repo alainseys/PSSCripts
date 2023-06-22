2925f268b5820542923ba2e12bd18dd1ea06276f
alain <alain.seys@outlook.com>
2023-06-22 14:05:30 +0200

    Toevoegen diverse scripts

---
+++ Office 365/O365_UserCreated_Last30Days.ps1
@@ -0,0 +1,7 @@
+﻿$createdSinceDate = ((Get-Date).AddDays(-31)).Date
+$ou = 'OU=Domain Policy,DC=BIOPACK,DC=BE'
+
+
+#Get-ADUser -Filter {whenCreated -ge $createdSinceDate} -Properties whenCreated -SearchBase $ou
+
+Get-ADUser -Filter {whenCreated -ge $createdSinceDate} -Properties whenCreated -SearchBase $ou | Select-Object userprincipalname, whencreated | Sort-Object whencreated | Export-Csv C:\biopack\skip-created-users.csv -NoTypeInformation
