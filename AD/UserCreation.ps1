31de449eb43cfeae24fee52922c9ffcd05bea5b0
alain <alain.seys@outlook.com>
2023-06-24 16:05:13 +0200

    documentatie toevoegen , voor proxy address

--- AD/UserCreation.ps1
+++ AD/UserCreation.ps1
@@ -5,7 +5,10 @@
 #Add user to multiple groups based on selection of department(eg sales, customerservers ...)
 #Example: $GroupName = @("group1","group2")
 
+
 # set default email and secondary email (proxyAddress)
+
+#Set-ADUser John.Doe -add @{ProxyAddresses="smtp:john.doe@domain.com"}
 
 #Invoke sheduled task
 #Invoke-Command -ComputerName xyz -ScriptBlock {schtask /run /tn "taskname"}
